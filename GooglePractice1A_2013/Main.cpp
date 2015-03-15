46160e99b015641ab877ac90cdd166dbf59fea74
Ryan Fore <ryan.b.fore@gmail.com>
2015-03-14 23:26:53 -0500

    Off by one error

--- GooglePractice1A_2013/Main.cpp
+++ GooglePractice1A_2013/Main.cpp
@@ -1,16 +1,24 @@
-#include <stdio.h>
+#define _USE_MATH_DEFINES
 
+#include <stdio.h>
+#include <math.h>
+
+
+int SolveProblem(long long sizeOfCircle, long long amountOfPaint, long long result);
+long double myPower(long double value, long double exp);
 
 int main()
 {
 	FILE * fileIn;
+	FILE * fileOut;
 	int numTestCases;
-	int sizeOfWhiteCircle;
-	int amountOfPaint;
+	long double sizeOfCircle = 0;
+	long double amountOfPaint = 0;
 
-	fileIn = fopen("D:\\TEMP\\Google\\A-small-practice.in", "r");
+	fileIn = fopen("D:\\TEMP\\Google\\Small.in", "r");
+	fileOut = fopen("D:\\TEMP\\Google\\res.out", "w");
 
-	if (fileIn == NULL)
+	if (fileIn == NULL || fileOut == NULL)
 	{
 		printf("Cannot open file\n");
 		getchar();
@@ -18,18 +26,53 @@ int main()
 	}
 
 	fscanf(fileIn, "%d", &numTestCases);
-	fscanf(fileIn, "%d %d", &sizeOfWhiteCircle, &amountOfPaint);
 
-	printf("Number of test cases: %d\n", numTestCases);
+	for (int i = 0; i < numTestCases; i++)
+	{
+		fscanf(fileIn, "%lf %lf", &sizeOfCircle, &amountOfPaint);
 
-	printf("size of white cirlce %d \n", sizeOfWhiteCircle);
-	printf("Amount of paint : %d", amountOfPaint);
+		long double areaToPaint = 0;
+		long long result = 0;
 
-	
+		amountOfPaint = amountOfPaint * (long double)M_PI;
 
-	getchar();
+		while (amountOfPaint > 0)
+		{
+			long double areaOfLastCircle = myPower(sizeOfCircle, (long double)2) * M_PI;
+			long double areaOfNewCircle = myPower((sizeOfCircle + 1), (long double)2) * M_PI;
+			areaToPaint = areaOfNewCircle - areaOfLastCircle;
+
+			if (areaToPaint <= amountOfPaint)
+			{
+				amountOfPaint = amountOfPaint - areaToPaint;
+				sizeOfCircle = sizeOfCircle + 2;
+				++result;
+			}
+			else
+			{
+				break;
+			}
+
+			
+		}
+		fprintf(fileOut, "Case #%d: %lld\n", i + 1, result);	
+	}
 
 	fclose(fileIn);
+	fclose(fileOut);
 	
 	return 1;
+}
+
+long double myPower(long double value, long double exp)
+{
+	long long index = 1;
+	long double newValue = value;
+	while (index < exp)
+	{
+		newValue = newValue * newValue;
+		++index;
+	}
+
+	return newValue;
 }
